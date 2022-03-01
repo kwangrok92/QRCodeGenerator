d60ac3081ab1119fa8b53bc533fbb63c97ae0f81
Cdeer <kwangrok92@naver.com>
2022-03-01 22:19:42 +0900

    Initial Commit

---
+++ QRCodeGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeGenerator
+//
+//  Created by 김광록 on 2022/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
